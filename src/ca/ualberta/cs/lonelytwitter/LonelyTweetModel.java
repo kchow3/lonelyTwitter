b191d777dcd046dd8e7111ee088fb954ccaf337b
kchow3 <kchow3@ualberta.ca>
2014-01-16 14:57:34 -0700

    Added some code.

--- src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
@@ -7,18 +7,34 @@ public class LonelyTweetModel {
 	private String text;
 	private Date timestamp;
 	
+	public LonelyTweetModel(String text, Date timestamp) {
+		super();
+		this.text = text;
+		this.timestamp = timestamp;
+	}
+	
+	public LonelyTweetModel(String text) {
+		super();
+		this.text = text;
+		this.timestamp = new Date();
+	}
+	
 	public String getText() {
 		return text;
 	}
+	
 	public void setText(String text) {
 		this.text = text;
 	}
+	
 	public Date getTimestamp() {
 		return timestamp;
 	}
+	
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
+	
 }
 
 
